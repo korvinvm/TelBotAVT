3e6fc50f42b1ae567377d8ddeee235d0fbebd572
korvinvm <mikhailvakhitov@gmail.com>
2022-06-02 04:47:35 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+from telebot import types
+token = "5407161599:AAHYHs9QL-fZ-5OkkqChkfnsaXZvFQCgc8c"
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+bot.infinity_polling()
+print ("all Ok")
