df6773347b3d62098c8039ad9e2a958011420db8
korvinvm <mikhailvakhitov@gmail.com>
2022-06-04 13:23:47 +0400

    eco

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from config import token
 from telebot import types
 print ("import Ok") 
 bot = telebot.TeleBot(token)
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
   bot.send_message(message.chat.id,"Привет ✌️ ")
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+
+#add button
+@bot.message_handler(commands = ['url'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_my_site= types.InlineKeyboardButton(text='Наш сайт', url='https://habrahabr.ru')
+    markup.add(btn_my_site)
+    bot.send_message(message.chat.id, "Нажми на кнопку и перейди на наш сайт.", reply_markup = markup)
+
+#@bot.send_message( )
+#@bot.message_handler(func=lambda m: True)
+#def echo_all(message):
+#	bot.reply_to(message, message.text)
 bot.infinity_polling()
 print ("all Ok") 
